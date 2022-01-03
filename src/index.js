c761cf9a6410b32744488f2eecedf8b479129d67
Janko Dedic <jankodedic7@gmail.com>
2022-01-03 15:43:30 +0100

    Set up React Router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom'
-import { App } from './components/App'
+import { AppRouter } from './components/AppRouter'
 
 const appDiv = document.getElementById('app')
-ReactDOM.render(<App />, app)
+ReactDOM.render(<AppRouter />, appDiv)
